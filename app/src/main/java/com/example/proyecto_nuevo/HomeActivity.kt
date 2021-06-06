c60794c11ea81ee1485aa48298e5329b336c0031
Braulio <braulioarguetamartinez3c2bda@gmail.com>
2021-06-05 23:28:30 -0500

    Mensaje1

---
+++ app/src/main/java/com/example/proyecto_nuevo/HomeActivity.kt
@@ -0,0 +1,59 @@
+package com.example.proyecto_nuevo
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType{
+
+    BASIC,
+    GOOGLE
+}
+
+class HomeActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val bundle :Bundle?=intent.extras
+        val email :String?=bundle?.getString("email")
+        val provider :String?=bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
+    }
+
+    private fun setup(email:String, provider:String){
+
+        title="Inicio"
+       // emailTextView.text=email
+        //providerTextView.text=provider
+
+
+        logOutButton.setOnClickListener {
+
+
+            val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
+    }
+
+
+}
